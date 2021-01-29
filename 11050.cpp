2dd986774f6b2810eb09ea5933a31d51e31f34ee
KwonYeKyeong <kwonliz6.6@gmail.com>
2021-01-29 21:56:43 +0900

    11050
    11050 : 이항 계수 1

--- 11050.cpp
+++ 11050.cpp
@@ -1,3 +1,4 @@
+/* 1锅 规过
 #include <iostream>
 using namespace std;
 
@@ -14,6 +15,37 @@ int main() {
 	}
 
 	cout << dp[k];
+
+	return 0;
+}
+*/
+
+// 2锅 规过
+#include <iostream>
+using namespace std;
+
+int dp[11][11];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	int n, k;
+
+	cin >> n >> k;
+
+	
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= i; j++) {
+			if (j == 0 || i == j) {
+				dp[i][j] = 1;
+				continue;
+			}
+			dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+		}
+	}
+
+	cout << dp[n][k];
 
 	return 0;
 }
