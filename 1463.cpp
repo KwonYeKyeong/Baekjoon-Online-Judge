81c2cd76dea12f331d21e2d5db93112735c3b523
KwonYeKyeong <kwonliz6.6@gmail.com>
2021-02-04 23:11:54 +0900

    1463
    1463 : 1로 만들기

--- 1463.cpp
+++ 1463.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int d[1000001];
-
-int min(int a, int b) {
-	return (a < b ? a : b);
-}
+int dp[1000001];
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);
+
 	int n;
+
 	cin >> n;
-	
+
 	for (int i = 2; i <= n; i++) {
-		d[i] = d[i - 1] + 1;
-		if (i % 3 == 0)
-			d[i] = min(d[i], d[i / 3] + 1);
+		if (i % 3 == 0) {
+			if (i % 2 == 0)
+				dp[i] = min(dp[i - 1], min(dp[i / 2], dp[i / 3])) + 1;
+			else dp[i] = min(dp[i - 1], dp[i / 3]) + 1;
+		}
 		else if (i % 2 == 0)
-			d[i] = min(d[i], d[i / 2] + 1);
+			dp[i] = min(dp[i - 1], dp[i / 2]) + 1;
+		else dp[i] = dp[i - 1] + 1;
 	}
-	cout << d[n];
+
+	cout << dp[n];
+
 	return 0;
 }
