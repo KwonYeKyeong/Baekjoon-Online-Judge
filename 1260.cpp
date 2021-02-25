609fdf9ffc1228166b27d7f1089f420a61f8de0c
KwonYeKyeong <kwonliz6.6@gmail.com>
2021-02-25 22:33:55 +0900

    update 1260.cpp
    1260 : DFS와 BFS

--- 1260.cpp
+++ 1260.cpp
@@ -1,3 +1,69 @@
+// vector 사용 ver.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int n, m, v, visited[1001], a, b;
+vector<int> vec[1001];
+
+bool compare(int a, int b) {
+	return a < b;
+}
+void dfs(int idx) {
+	visited[idx] = 1;
+	cout << idx << ' ';
+
+	for (int v : vec[idx]) {
+		if (!visited[v])
+			dfs(v);
+	}
+}
+void bfs() {
+	queue<int> q;
+
+	q.push(v);
+	visited[v] = 1;
+	while (!q.empty()) {
+		int qq = q.front();
+		cout << qq << ' ';
+		q.pop();
+
+		for (int v : vec[qq]) {
+			if (!visited[v]) {
+				q.push(v);
+				visited[v] = 1;
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m >> v;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		vec[a].push_back(b);
+		vec[b].push_back(a);
+	}
+
+	for (int i = 1; i<1001; i++)
+		sort(vec[i].begin(), vec[i].end(), compare);
+	dfs(v);
+
+	cout << '\n';
+
+	memset(visited, 0, sizeof(visited));
+	bfs();
+
+	return 0;
+}
+
+/* vector 미사용 ver.
 #include <iostream>
 #include <queue>
 using namespace std;
@@ -55,3 +121,4 @@ int main() {
 
 	return 0;
 }
+*/
